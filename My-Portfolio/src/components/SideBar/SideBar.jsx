5df31b62133e7f3f346be8223cda966d9f247594
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-11 15:33:33 +0530

    add Mylogo image and name to Sidebar

--- My-Portfolio/src/components/SideBar/SideBar.jsx
+++ My-Portfolio/src/components/SideBar/SideBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './SideBar.css';
+import Mylogo from '../../assets/Mylogo.png';
+
 
 export default function SideBar() {
   return (
-    <div>
-        <h1>this is side bar</h1>
+    <div className='sideBarMain'>
+      <div className='sideBarContent'>
+          <div className='SBmylogo'>
+            <img className='SideBarMylogo' src={Mylogo}  />
+          </div>
+          <h1 className='MynameSB'>Nimesh Madhuwantha</h1>
+          <div className='MyOccupation'>
+           <h5 >Full Stack Developer</h5>
+           <h5 className='Myoccupation2'>UI/UX Engineer</h5>
+          </div>
+          
+      </div>
     </div>
   )
 }
