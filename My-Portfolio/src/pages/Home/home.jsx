f19cd13903b95cf3867e2af12ad16cbdd5869e27
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-16 23:57:43 +0530

    update the home page front text

--- My-Portfolio/src/pages/Home/home.jsx
+++ My-Portfolio/src/pages/Home/home.jsx
@@ -3,12 +3,21 @@ import './Home.css'
 
 
 function Home() {
+  
+
+
   return (
     <div>
-      
       <div className='HomeMainContent'>
-        
-        
+        <div className='HomeTopText'>
+          <h1>Hi, I'm <span className='NimeshTopText'> Nimesh !</span></h1>
+            <div className='ChangingText'>
+              <span className='FSDtop'>Full Stack Developer</span>
+              <span className='FSDtop'>UI/UX Engineer</span>
+              <span className='FSDtop'>Full Stack Developer</span>
+              <span className='FSDtop'>UI/UX Engineer</span>
+            </div>
+        </div>  
       </div>
     </div>
   );
