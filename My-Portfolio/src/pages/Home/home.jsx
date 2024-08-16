d2bade1336170ec82e8bab239954438317245efc
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-17 01:33:24 +0530

    insert MyPhoto

--- My-Portfolio/src/pages/Home/home.jsx
+++ My-Portfolio/src/pages/Home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css'
-
+import MyPhoto from '../../assetS/MyPhoto.png';
 
 function Home() {
   
@@ -17,7 +17,21 @@ function Home() {
               <span className='FSDtop'>Full Stack Developer</span>
               <span className='FSDtop'>UI/UX Engineer</span>
             </div>
+            <div className='HomeTopDescription'>
+              <h2>
+              <span className='HomeWelcomeText'>Welcome to my portfolio! </span><br />
+              I am a UI/UX engineer, UI/UX designer, full-stack developer, and artist. 
+              My work is a fusion of technical precision and creative imagination. 
+              From intuitive user interfaces and robust web solutions to captivating artwork, 
+              each piece reflects my passion for crafting seamless, visually stunning digital experiences. 
+              Dive into my world of innovation and creativity.
+              </h2>
+            </div>
+        </div>
+        <div className='HomeMyPhoto'>
+          <img src={MyPhoto} />
         </div>  
+
       </div>
     </div>
   );
