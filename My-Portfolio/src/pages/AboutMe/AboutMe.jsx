a2070fda30d75e84b53f388ab3127f5700f37429
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-17 18:55:37 +0530

    create About Me page and top text

--- My-Portfolio/src/pages/AboutMe/AboutMe.jsx
+++ My-Portfolio/src/pages/AboutMe/AboutMe.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './AboutMe.css'
 
 export default function AboutMe() {
   return (
     <div>
-     
-      <h1>thi s my about page</h1>
-    </div>
+      <div className='About_MainContent'>
+        <div className='About_TopContent'>
+          <h1 className='About_Nice_to_meet_Text'>NICE TO MEET YOU!</h1>
+          <h1 className='About_MyName'>Nimesh Madhuwantha</h1>
+          <h1 className='About_MyOccupation'>UIUX Engineer <br />
+          Full Stack Developer</h1>
+
+        </div>
+        
+
+      </div>
+     </div>
   )
 }
