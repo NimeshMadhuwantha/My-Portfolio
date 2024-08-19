aab22ea7c6c0781c942e128cb189718ad8a07738
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-19 18:36:08 +0530

    create Contact page form

--- My-Portfolio/src/pages/Contact/Contact.jsx
+++ My-Portfolio/src/pages/Contact/Contact.jsx
@@ -36,6 +36,70 @@ export default function Contact() {
             </div>
 
           </div>
+          <div className='Contact_Form'>
+           
+           <form className="Contact_Form_Input"> 
+            <div className='Contact_Name_Input'>
+           <input 
+            type="text" 
+            id="name" 
+            aria-describedby="helper-text-explanation" 
+            className="bg-white text-gray-900 text-sm rounded-lg  block w-[480px] p-2.5
+             placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
+            placeholder="Name"
+            style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)'}}
+            
+            />
+            </div>
+
+          <div className='Contact_Email_Input'>
+          <input 
+            type="email" 
+            id="email" 
+            aria-describedby="helper-text-explanation" 
+            className="bg-white text-gray-900 text-sm rounded-lg block w-[480px] p-2.5
+             placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
+            placeholder="Email"
+            style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)' }}
+           />
+          </div> 
+          <div className='Contact_Message_Input'>
+            <textarea
+            id="message"
+            aria-describedby="helper-text-explanation"
+            className="bg-white text-gray-900 text-sm rounded-lg  block w-[480px] h-[100px] p-2.5
+             placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]"
+            placeholder="Write your Message here"
+            style={{ 
+            boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)',
+            resize: 'none'
+            }}
+            ></textarea>
+
+             <div className='Contact_Send_Button'>
+             <button className="bg-[#023E57] hover:bg-transparent text-white font-semibold 
+             hover:text-[#023E57] border-4 border-[#023E57] 
+             rounded-lg transition-all duration-1000"
+             style={{ fontSize: '15px', width: '190px', height: '55px' }}>
+             Send Message 
+             </button>
+             </div>
+
+
+          </div>
+
+
+             
+            </form>
+
+             
+
+
+           
+          </div>
+
+
+          
 
         </div>
       
