8f41857a6c3fdf5fa3dedd55d7975f6a393d821d
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-08-20 23:49:54 +0530

    link the cards to behance and github

--- My-Portfolio/src/components/UIUXProjects/UIUXProject.jsx
+++ My-Portfolio/src/components/UIUXProjects/UIUXProject.jsx
@@ -12,37 +12,49 @@ const UIUXProject = () => {
             <div className='UIUX_Content'>
             <div className='UIUX_Cards'>
                 <div className='UIUX_Card1'>
+                <a href="https://www.behance.net/gallery/199603389/Health-care-Mobile-app-UIUX" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={HealthCareApp} alt="" />
                     <h1>Health care Mobile app  <br />
                     UIUX</h1>
                     <h2>Figma | Photoshop</h2>
+                </a>
                 </div>
                 <div className='UIUX_Card1'>
+                <a href="https://www.behance.net/gallery/199746819/Pizza-Mobile-App-UIUX" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={PizzaApp} alt="" />
                     <h1>Pizza Mobile app  <br />
                     UIUX</h1>
                     <h2>Figma | Photoshop</h2>
+                </a>
                 </div>
                 <div className='UIUX_Card1'>
+                <a href="https://www.behance.net/gallery/205631415/My-Portfolio-Web-Site" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={ShoppingApp} alt="" />
                     <h1>Shopping Mobile app <br />
                     UIUX </h1>
                     <h2>Figma | Photoshop</h2>
+                </a>
                 </div>
 
             </div>
             <div className='UIUX_Cards'>
                 <div className='UIUX_Card1'>
+                <a href="https://www.behance.net/gallery/205631415/My-Portfolio-Web-Site" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={MyPortfolio} alt="" />
                     <h1>My Portfolio Web app  <br />
                     UIUX </h1>
                     <h2>Figma | Photoshop</h2>
+                    </a>
                 </div>
                 <div className='UIUX_Card1'>
                 </div>
                 <div className='UIUX_Card1'>
                 </div>
-
+                
             </div>
             <div className='UIUX_Cards'>
                 <div className='UIUX_Card1'>
--- My-Portfolio/src/components/WebDevelopmentProject/WebDevelopment.jsx
+++ My-Portfolio/src/components/WebDevelopmentProject/WebDevelopment.jsx
@@ -12,31 +12,43 @@ const WebDevelopment = () => {
             <div className='WebDev_Content'>
             <div className='WebD_Cards'>
                 <div className='WebD_Card1'>
+                <a href="https://github.com/NimeshMadhuwantha/My-Portfolio.git" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={MyPortfolio} alt="" />
                     <h1>Web Application <br />
                     My Portfolio</h1>
                     <h2>JavaScript | React</h2>
+                </a>
                 </div>
                 <div className='WebD_Card1'>
+                <a href="https://github.com/NimeshMadhuwantha/WeatherApp.git" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={WeatherApp} alt="" />
                     <h1>Weather Application <br />
                     Python</h1>
                     <h2>Python | React</h2>
+                </a>
                 </div>
                 <div className='WebD_Card1'>
+                <a href="https://github.com/NimeshMadhuwantha/currency-exchange-basic-web-application.git" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={CurrencyExchange} alt="" />
                     <h1>Currency Convert <br />
                     Web Application </h1>
                     <h2>JavaScript | React</h2>
+                </a>
                 </div>
 
             </div>
             <div className='WebD_Cards'>
                 <div className='WebD_Card1'>
+                <a href="https://github.com/NimeshMadhuwantha/GameSeek.git" target="_blank" rel="noopener noreferrer" 
+                style={{ display: 'block', textDecoration: 'none', color: 'inherit' }}>
                     <img src={GameSeek} alt="" />
                     <h1>GameSeek  <br />
                     Web Application </h1>
                     <h2>JavaScript | React</h2>
+                </a>
                 </div>
                 <div className='WebD_Card1'>
                 </div>
