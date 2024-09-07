0553cee3fc5e5b0a998a4533a8f9b19242444299
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-09-07 10:51:51 +0530

    responsive the contact page

--- My-Portfolio/src/pages/Contact/Contact.jsx
+++ My-Portfolio/src/pages/Contact/Contact.jsx
@@ -111,7 +111,9 @@ export default function Contact() {
 
       <div className='Contact_MiddleContent'>
         <div className='Contact_Details'>
-          <h1>Contact Information</h1>
+          <div className='Contact_Info_div'>
+          <h1 className='Contact_Info'>Contact Information</h1>
+          </div>
           <h2>Put me a message and connect with me—let's collaborate! Reach out via email or through my social profiles</h2>
           <div className='Contact_Icons'>
             <div className='Contact_Call'>
@@ -130,71 +132,81 @@ export default function Contact() {
         </div>
 
         <div className='Contact_Form'>
-          <form className="Contact_Form_Input" ref={form} onSubmit={sendEmail}> 
-            <div className='Contact_Name_Input'>
-              <input 
-                type="text" 
-                name="from_name"
-                className="bg-white text-gray-900 text-sm rounded-lg block w-[480px] p-2.5
-                placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
-                placeholder="Name"
-                style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)'}}
-                onChange={validateForm}
-              />
-            </div>
+        <form className="Contact_Form_Input" ref={form} onSubmit={sendEmail}> 
+          <div className='Contact_Name_Input'>
+            <input 
+              type="text" 
+              name="from_name"
+              className="bg-white text-gray-900 text-sm rounded-lg block w-full p-2.5
+              placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
+              placeholder="Name"
+              style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)' }}
+              onChange={validateForm}
+            />
+          </div>
 
-            <div className='Contact_Email_Input'>
-              <input 
-                type="email" 
-                name="from_email"
-                className="bg-white text-gray-900 text-sm rounded-lg block w-[480px] p-2.5
-                placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
-                placeholder="Email"
-                style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)' }}
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  validateForm();
-                }}
-              />
-            </div> 
+          <div className='Contact_Email_Input'>
+            <input 
+              type="email" 
+              name="from_email"
+              className="bg-white text-gray-900 text-sm rounded-lg block w-full p-2.5
+              placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
+              placeholder="Email"
+              style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)' }}
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                validateForm();
+              }}
+            />
+          </div> 
 
-            <div className='Contact_Email_Input'>
-              <input 
-                type="email" 
-                name="re_email"
-                className="bg-white text-gray-900 text-sm rounded-lg block w-[480px] p-2.5
-                placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
-                placeholder="Re-enter Email"
-                style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)' }}
-                value={reEmail}
-                onChange={(e) => {
-                  setReEmail(e.target.value);
-                  validateForm();
-                }}
-              />
-            </div> 
+          <div className='Contact_Email_Input'>
+            <input 
+              type="email" 
+              name="re_email"
+              className="bg-white text-gray-900 text-sm rounded-lg block w-full p-2.5
+              placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]" 
+              placeholder="Re-enter Email"
+              style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)' }}
+              value={reEmail}
+              onChange={(e) => {
+                setReEmail(e.target.value);
+                validateForm();
+              }}
+            />
+          </div> 
 
-            <div className='Contact_Message_Input'>
-              <textarea
-                name="message"
-                className="bg-white text-gray-900 text-sm rounded-lg block w-[480px] h-[100px] p-2.5
-                placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]"
-                placeholder="Write your Message here"
-                style={{ 
-                  boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)',
-                  resize: 'none'
-                }}
-                onChange={validateForm}
-              ></textarea>
+          <div className='Contact_Message_Input'>
+            <textarea
+              name="message"
+              className="bg-white text-gray-900 text-sm rounded-lg block w-full h-[100px] p-2.5
+              placeholder-gray-500 pl-7 focus:outline-none focus:ring-2 focus:ring-[#023E57]"
+              placeholder="Write your Message here"
+              style={{ 
+                boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)',
+                resize: 'none'
+              }}
+              onChange={validateForm}
+            ></textarea>
 
               <div className='Contact_Send_Button'>
                 <button 
                   type="submit" 
                   className="bg-[#023E57] hover:bg-transparent text-white font-semibold 
                   hover:text-[#023E57] border-4 border-[#023E57] 
-                  rounded-lg transition-all duration-1000"
-                  style={{ fontSize: '15px', width: '190px', height: '55px' }}
+                  rounded-lg transition-all duration-1000
+                   xl:w-[210px] xl:h-[50px] 
+                   lg:w-[185px] lg:h-[41px] 
+                   md:w-[170px] md:h-[40px] 
+                   sm:w-[155px] sm:h-[40px] 
+                   w-[125px] h-[34px]   
+                   xl:text-[1rem] 
+                   lg:text-[1rem]  
+                   md:text-[0.8rem] 
+                   sm:text-[0.7rem] 
+                   text-[0.5rem]  
+                  "  
                 >
                   Send Message 
                 </button>
